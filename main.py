ceb16fdc7bef1ef03c1020b9d9985d92b1f09b72
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2024-11-19 14:28:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ from langchain_openai import ChatOpenAI
 import base64
 from datetime import datetime
 from crewai_tools import FileReadTool, WebsiteSearchTool
-
-from dotenv import load_dotenv
-
-load_dotenv()  # Load environment variables from .env
-
 import os
 
-# Access the keys
-openai_api_key = os.getenv("OPENAI_API_KEY")
-serper_api_key = os.getenv("SERPER_API_KEY")
+# Configuração do ambiente da API
+api_key = os.getenv["OPENAI_API_KEY"]
 
 # Inicializa o modelo LLM com OpenAI
 modelo_linguagem = ChatOpenAI(
@@ -66,6 +60,8 @@ if login():
         publico_alvo = st.text_input('Público-Alvo:', key="publico_alvo")
         concorrentes = st.text_input('Concorrentes:', key="concorrentes")
         site_concorrentes = st.text_input('Site dos Concorrentes:', key="site_concorrentes")
+        objetivos_de_marca = st.text_input('Objetivos de marca', key="objetivos_marca")
+        referencia_da_marca = st.text_input('O que a marca faz, quais seus diferenciais, seus objetivos, quem é a marca?', key = "referencias_marca")
 
         # Se o relatório já foi gerado, exiba os resultados
         if "relatorio_gerado" in st.session_state and st.session_state.relatorio_gerado:
@@ -88,8 +84,8 @@ if login():
                     agentes = [
                     Agent(
                         role="Líder e revisor geral de estratégia",
-                        goal=f"Revisar toda a estratégia de {nome_cliente} e garantir alinhamento com os objetivos de marca e o público-alvo '{publico_alvo}'.",
-                        backstory=f"Você é Philip Kotler, renomado estrategista de marketing, liderando o planejamento de {nome_cliente} no ramo de {ramo_atuacao}.",
+                        goal=f"Revisar toda a estratégia de {nome_cliente} e garantir alinhamento com os {objetivos_de_marca}, o público-alvo {publico_alvo} e as {referencia_da_marca}.",
+                        backstory=f"Você é Philip Kotler, renomado estrategista de marketing, usando todo o seu conhecimento avançado em administração de marketing, liderando o planejamento de {nome_cliente} no ramo de {ramo_atuacao}.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     ),
@@ -102,35 +98,35 @@ if login():
                     ),
                     Agent(
                         role="Criador do posicionamento de marca",
-                        goal=f"Criar o posicionamento adequado para {nome_cliente}, considerando o público-alvo '{publico_alvo}'.",
+                        goal=f"Criar o posicionamento de marca adequado para {nome_cliente}, considerando o público-alvo {publico_alvo}, o {objetivos_de_marca} a análise SWOT, e o Golden Circle e a referencia de marca: {referencia_da_marca}.",
                         backstory="Você é Al Ries, responsável por desenvolver o posicionamento de marca.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     ),
                     Agent(
                         role="Criador do Golden Circle",
-                        goal=f"Desenvolver o Golden Circle para {nome_cliente}, considerando o público-alvo '{publico_alvo}'.",
+                        goal=f"Desenvolver o Golden Circle para {nome_cliente}, considerando o público-alvo '{publico_alvo}', SWOT, {objetivos_de_marca} e a referencia de marca: {referencia_da_marca}.", 
                         backstory="Você é Simon Sinek, desenvolvendo o Golden Circle.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     ),
                     Agent(
                         role="Criador da Brand Persona",
-                        goal=f"Definir a Brand Persona de {nome_cliente}, garantindo consistência na comunicação.",
+                        goal=f"Definir a Brand Persona de {nome_cliente}, garantindo consistência na comunicação e levando em conta o objetivo de marca: {objetivos_de_marca} e a referencia de marca: {referencia_da_marca}.",
                         backstory="Você é Marty Neumeier, criando a Brand Persona.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     ),
                     Agent(
                         role="Criador da Buyer Persona e Público-Alvo",
-                        goal=f"Definir a buyer persona e o público-alvo de {nome_cliente}.",
+                        goal=f"Definir a buyer persona e o público-alvo de {nome_cliente} levando em conta o objetivo de marca: {objetivos_de_marca} e a referencia de marca: {referencia_da_marca}, os {concorrentes} o posicionamento de marca e o golden circle.",
                         backstory="Você é Adele Revella, conduzindo a criação da buyer persona.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     ),
                     Agent(
                         role="Criador da Matriz SWOT",
-                        goal=f"Desenvolver uma análise SWOT para {nome_cliente} considerando os concorrentes '{concorrentes}'.",
+                        goal=f"Desenvolver uma análise SWOT para {nome_cliente} considerando os concorrentes '{concorrentes}, o objetivo de marca: {objetivos_de_marca} e a referencia de marca: {referencia_da_marca}.", 
                         backstory="Você é Michael Porter, desenvolvendo a análise SWOT.",
                         allow_delegation=False,
                         llm=modelo_linguagem
@@ -138,7 +134,7 @@ if login():
                     Agent(
                         role="Criador do Tom de Voz",
                         goal=f"Definir o tom de voz de {nome_cliente}.",
-                        backstory="Você é Ann Handley, desenvolvendo a voz da marca.",
+                        backstory="Você é Ann Handley, desenvolvendo a voz da marca o objetivo de marca: {objetivo de marca} e a referencia de marca: {referencia_de_marca} o posicionamento, brand persona e golden circle.",
                         allow_delegation=False,
                         llm=modelo_linguagem
                     )
@@ -146,11 +142,12 @@ if login():
 
                 # Criando tarefas correspondentes aos agentes
                 tarefas = [
+                    
                     Task(
-                        description="Revisar a estratégia geral.",
-                        expected_output="Revisão detalhada da estratégia considerando o público-alvo em português.",
-                        agent=agentes[0],
-                        output_file = 'revisao.md'
+                        description="Criar a Matriz SWOT.",
+                        expected_output="Análise SWOT completa em formato de tabela.",
+                        agent=agentes[6],
+                        output_file = 'SWOT.md'
                     ),
                     Task(
                         description="Criar o posicionamento de marca.",
@@ -166,25 +163,19 @@ if login():
                     ),
                     Task(
                         description="Criar a Brand Persona.",
-                        expected_output="Brand Persona detalhada, alinhada com a marca do cliente.",
+                        expected_output=f"Brand Persona detalhada, alinhada com a marca do {nome_cliente}.",
                         agent=agentes[4],
                         output_file = 'BP.md'
                     ),
                     Task(
                         description="Definir a Buyer Persona e o Público-Alvo.",
-                        expected_output="Descrição detalhada da buyer persona e do público-alvo.",
+                        expected_output="Descrição detalhada da buyer persona e do público-alvo com os seguintes atributos enunciados: nome fictício, idade, gênero, classe social, objetivos, dores, vontades.", 
                         agent=agentes[5],
                         output_file = 'BuyerP.md'
                     ),
                     Task(
-                        description="Criar a Matriz SWOT.",
-                        expected_output="Análise SWOT completa em formato de tabela.",
-                        agent=agentes[6],
-                        output_file = 'SWOT.md'
-                    ),
-                    Task(
                         description="Definir o Tom de Voz.",
-                        expected_output="Descrição do tom de voz, incluindo nuvem de palavras e palavras proibidas.",
+                        expected_output="Descrição do tom de voz, na {pessoa}, incluindo nuvem de palavras e palavras proibidas. Retorne entre 3 a 5 adjetivos que definem o tom com suas respectivas explicações. ex: 'tom é amigavel, para transparecer uma relação de confiança' com frases de exemplo de aplicação do tom.",
                         agent=agentes[7],
                         output_file = 'TV.md'
                     ),
@@ -193,7 +184,12 @@ if login():
                         expected_output="Análise PEST com pelo menos 5 pontos em cada etapa.",
                         agent=agentes[1],
                         output_file = 'pest.md'
-                    )
+                    ),
+                    Task(
+                        description="Revisar a estratégia geral.",
+                        expected_output="Revisão detalhada de cada uma das tarefas realizadas pelos agentes levando em conta os princípios de marketing para entender se há ponto de melhoria para objermos uma estratégia assertiva de acordo com os {objetivos  de marca} do {cliente} considerando o público-alvo em português.",
+                        agent=agentes[0],
+                        output_file = 'revisao.md')
                 ]
 
                 # Processo do Crew
@@ -213,6 +209,5 @@ if login():
                 for tarefa in tarefas:
                     st.markdown(f"**Arquivo**: {tarefa.output_file}")
                     st.markdown(tarefa.output.raw)
-
 
     
