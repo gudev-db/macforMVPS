7aa75cd0be242234f805e29e80cd44bbba52b607
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2024-11-22 19:08:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ def limpar_estado():
 
 # Função de login
 file_tool = FileReadTool()
+
+
 def login():
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
@@ -70,7 +72,7 @@ if login():
         st.subheader("Suba os Arquivos Estratégicos de atualidades para a análise PEST (CSV, PDF ou DOCX)")
         pest_files = st.file_uploader("Escolha arquivos", type=["csv", "pdf", "docx"], accept_multiple_files=True)
 
-    if uploaded_file is not None:
+        if uploaded_file is not None:
       
 
         # Se o relatório já foi gerado, exiba os resultados
