72b5f8feeb4ba6552adc70b64e4b8eabf916fa3a
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2024-11-26 22:36:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,8 @@ from crewai_tools import BaseTool, tool
 file_tool = PDFSearchTool()
 
 st.image('Screenshot Capture - 2024-11-26 - 20-28-31.png', width=150)
-        st.title('Macfor AI Solutions')
-
-        # Inputs do cliente
-        st.text('Empoderada por IA, a Macfor conta com um sistema gerador de documentos automatizado. Preencha o campo abaixo e gere um documento de planejamento estratégico para poupar tempo dos seus analistas de marketing e otimizar o tempo de sua equipe. Foque o seu trabalho em seu diferencial humano e automatize tarefas repetitivas!')
+st.title('Macfor AI Solutions')
+st.text('Empoderada por IA, a Macfor conta com um sistema gerador de documentos automatizado. Preencha o campo abaixo e gere um documento de planejamento estratégico para poupar tempo dos seus analistas de marketing e otimizar o tempo de sua equipe. Foque o seu trabalho em seu diferencial humano e automatize tarefas repetitivas!')
 
 
 def login():
