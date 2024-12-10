171f05e4bad9dbf44d90cec5b49fde6b2826c959
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2024-12-10 12:52:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 from tavily import TavilyClient
 
 with open( "style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= False)
 
 
 # Configuração do ambiente da API
