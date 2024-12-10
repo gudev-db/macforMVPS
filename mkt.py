ac445fedb944e5b5344cb8332f06cc153a052dd5
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2024-12-10 12:49:15 -0300

    Update mkt.py

--- mkt.py
+++ mkt.py
@@ -60,7 +60,7 @@ def planej_mkt_page():
     )
     referencia_da_marca = st.text_input('O que a marca faz, quais seus diferenciais, seus objetivos, quem é a marca?', key="referencias_marca", placeholder="Ex: A marca X oferece roupas sustentáveis com foco em conforto e estilo.")
     
-    st.subheader("Suba os Arquivos Estratégicos (PDF) (Único ou múltiplos)")
+    st.subheader("(Opcional) Suba os Arquivos Estratégicos (PDF) (Único ou múltiplos)")
     pest_files = st.file_uploader("Escolha arquivos de PDF para referência de mercado", type=["pdf"], accept_multiple_files=True)
    
 
