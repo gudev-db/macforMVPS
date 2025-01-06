391a41e7b5c2daaed0b31b43dda81fca0cb1a511
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-06 14:09:02 -0300

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -28,21 +28,51 @@ def visualizar_planejamentos():
     selected_planejamento = collection.find_one({"id_planejamento": selected_id})
 
     if selected_planejamento:
-        # Display the selected planejamento
+        # Exibindo o nome do cliente
         st.subheader(f"Planejamento para: {selected_planejamento.get('cliente')}")
         
-        
-        # Exibindo as informações dos campos de planejamento (como SWOT, GC, etc.)
+        # Exibindo as seções do planejamento com subheaders e os dados respectivos
+        st.header('1. Etapa de Pesquisa de Mercado')
+        st.subheader('1.1 Análise SWOT')
         st.markdown(f"**SWOT:** {selected_planejamento.get('SWOT')}")
-        st.markdown(f"**GC:** {selected_planejamento.get('GC')}")
-        st.markdown(f"**Posicionamento de Marca:** {selected_planejamento.get('Posicionamento_Marca')}")
-        st.markdown(f"**Brand Persona:** {selected_planejamento.get('Brand_Persona')}")
-        st.markdown(f"**Buyer Persona:** {selected_planejamento.get('Buyer_Persona')}")
-        st.markdown(f"**Tom de Voz:** {selected_planejamento.get('Tom_Voz')}")
+        
+        st.subheader('1.2 Análise PEST')
         st.markdown(f"**PEST:** {selected_planejamento.get('PEST')}")
-        st.markdown(f"**Revisão:** {selected_planejamento.get('Revisao')}")
-        st.markdown(f"**Estratégia de Conteúdo:** {selected_planejamento.get('Estrategia_Conteudo')}")
+        
+        st.header('2. Etapa de Estratégica')
+        st.subheader('2.1 Golden Circle')
+        st.markdown(f"**GC:** {selected_planejamento.get('GC')}")
+        
+        st.subheader('2.2 Posicionamento de Marca')
+        st.markdown(f"**Posicionamento de Marca:** {selected_planejamento.get('Posicionamento_Marca')}")
+        
+        st.subheader('2.3 Brand Persona')
+        st.markdown(f"**Brand Persona:** {selected_planejamento.get('Brand_Persona')}")
+        
+        st.subheader('2.4 Buyer Persona')
+        st.markdown(f"**Buyer Persona:** {selected_planejamento.get('Buyer_Persona')}")
+        
+        st.subheader('2.5 Tom de Voz')
+        st.markdown(f"**Tom de Voz:** {selected_planejamento.get('Tom_Voz')}")
+        
+        st.header('3. Etapa de Planejamento de Mídias')
+        st.subheader('3.1 Plano para Criativos')
+        st.markdown(f"**Criativos:** {selected_planejamento.get('Criativos')}")
+        
+        st.subheader('3.2 Plano de SEO')
         st.markdown(f"**Plano SEO:** {selected_planejamento.get('Plano_SEO')}")
+        
+        st.subheader('3.3 Plano de CRM')
+        st.markdown(f"**Plano CRM:** {selected_planejamento.get('Plano_CRM')}")
+        
+        st.subheader('3.4 Plano de Design/Marca')
+        st.markdown(f"**Plano Design/Marca:** {selected_planejamento.get('Plano_Design_Marca')}")
+        
+        st.subheader('3.5 Estratégia de Conteúdo')
+        st.markdown(f"**Estratégia de Conteúdo:** {selected_planejamento.get('Estrategia_Conteudo')}")
+        
+        st.subheader('3.6 Revisão')
+        st.markdown(f"**Revisão:** {selected_planejamento.get('Revisao')}")
     else:
         st.write("Planejamento não encontrado.")
 
