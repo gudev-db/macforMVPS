4cab2e3f766ae53c2776ae972c84d208d2c4dc0c
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-08 08:43:08 -0300

    Update mkt.py

--- mkt.py
+++ mkt.py
@@ -162,7 +162,9 @@ def planej_mkt_page():
                                 goal=f'''Aprenda sobre análise PEST em {pest_files}. Realizar a análise PEST para o cliente {nome_cliente} em português brasileiro.''',
                                 backstory=f'''Você é Philip Kotler, liderando a análise PEST para o planejamento estratégico de {nome_cliente} em português brasileiro. 
                                 Levando em conta as informações coletadas em {politic}, {economic}, {social} e {tec} realize a análise PEST, essas informações são o que a sua 
-                                análise PEST deve se basear em.''',
+                                análise PEST deve se basear em. Você está realizando essa análise PEST para o cliente {nome_cliente} que é do setor de atuação {ramo_atuacao}.
+                                O intuito do planejamento estratégico está explicitado em {intuito_plano}. Você possui uma vasta experiência em desenvolver análises PEST relevantes,
+                                perspicazes e detalhadas. Você sabe exatamente como extrair informações relevantes para o crescimento de seus clientes.''',
                                 allow_delegation=False,
                                 llm=modelo_linguagem,
                                 tools=[]
@@ -345,7 +347,8 @@ def planej_mkt_page():
                                 ),
                                 Task(
                                     description="Criar a Brand Persona.",
-                                    expected_output=f'''Brand Persona detalhada, alinhada com a marca do {nome_cliente} em português brasileiro considerando o seguinte contexto (texto raspado do site do cliente {nome_cliente}) :{website_all_texts}
+                                    expected_output=f'''Brand Persona detalhada, alinhada com a marca do {nome_cliente} que é do setor de atuação {ramo_atuacao} em português brasileiro considerando o 
+                                    seguinte contexto (texto raspado do site do cliente {nome_cliente}) :{website_all_texts}
                                     , e o objetivo do planejamento estratégico {intuito_plano}. 
                                     
                                     -Defina seu gênero, faixa de idade, qual a sua bagagem, defina sua personalidade. 
