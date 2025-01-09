fb9a7fdb84ca60d41f04a2e2b36a3cdc2e4ed80c
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-09 13:57:57 -0300

    Update mkt.py

--- mkt.py
+++ mkt.py
@@ -12,7 +12,7 @@ import os
 from tavily import TavilyClient
 from pymongo import MongoClient
 import SEOtools
-from equipe import agentes
+#from equipe import agentes
 
 
 
@@ -137,6 +137,161 @@ def planej_mkt_page():
                     st.write("Por favor, preencha todas as informações do cliente.")
                 else:
                     with st.spinner('Gerando o planejamento...'):
+
+
+                        agentes = [
+                            Agent(
+                                role="Líder e revisor geral de estratégia",
+                                goal=f'''Aprenda sobre revisão geral de estratégia em {pest_files}. Revisar toda a estratégia de {nome_cliente} e garantir 
+                                alinhamento com os {objetivos_de_marca}, o público-alvo {publico_alvo} e as {referencia_da_marca}.''',
+                                backstory=f'''Você é Philip Kotler, renomado estrategista de marketing, usando todo o seu conhecimento 
+                                avançado em administração de marketing como nos documentos de {pest_files}, liderando o planejamento de {nome_cliente} no 
+                                ramo de {ramo_atuacao} em português brasileiro.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Analista PEST",
+                                goal=f'''Aprenda sobre análise PEST em {pest_files}. Realizar a análise PEST para o cliente {nome_cliente} em português brasileiro.''',
+                                backstory=f'''Você é Philip Kotler, liderando a análise PEST para o planejamento estratégico de {nome_cliente} em português brasileiro. 
+                                Levando em conta as informações coletadas em {politic}, {economic}, {social} e {tec} realize a análise PEST, essas informações são o que a sua 
+                                análise PEST deve se basear em. Você está realizando essa análise PEST para o cliente {nome_cliente} que é do setor de atuação {ramo_atuacao}.
+                                O intuito do planejamento estratégico está explicitado em {intuito_plano}. Você possui uma vasta experiência em desenvolver análises PEST relevantes,
+                                perspicazes e detalhadas. Você sabe exatamente como extrair informações relevantes para o crescimento de seus clientes.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Analista SWOT",
+                                goal=f'''Aprenda sobre análise SWOT e crie a análise para {nome_cliente}, com base nos dados de mercado disponíveis.''',
+                                backstory='''Você é um analista de marketing focado em realizar uma análise SWOT completa com dados extraídos de fontes diversas, como documentos PDF e CSV.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Especialista em Matriz BCG",
+                                goal=f"Desenvolver a Matriz BCG para o {nome_cliente}, com base nas informações do mercado e concorrência disponíveis.",
+                                backstory="Você é um especialista em estratégia de negócios e está ajudando a construir a Matriz BCG com base nos dados de mercado disponíveis, incluindo concorrentes.",
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Consultor de Pricing",
+                                goal=f'''Analisar a estratégia de preços para {nome_cliente}, utilizando dados de mercado e concorrência.''',
+                                backstory=f'''Você é um consultor de pricing experiente e ajudará {nome_cliente} a entender as melhores práticas
+                                de precificação com base na análise de mercado e concorrência.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Analista de Segmentação de Mercado",
+                                goal=f'''Segmentar o mercado para {nome_cliente} com base nos dados de concorrentes e no perfil do público-alvo.''',
+                                backstory=f'''Você é um analista de mercado com a missão de segmentar o público de {nome_cliente} e gerar insights acionáveis
+                                para o planejamento de marketing.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Criador de Persona",
+                                goal=f'''Desenvolver personas para o {nome_cliente} com base nos dados de público-alvo e concorrência.''',
+                                backstory=f'''Você é um especialista em marketing digital, com o objetivo de criar personas detalhadas para 
+                                {nome_cliente}, que ajudem a direcionar a comunicação de marketing.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Estratégia de Mídia Social",
+                                goal=f'''Desenvolver uma estratégia de mídia social para {nome_cliente} com base nas análises de mercado e público-alvo.''',
+                                backstory=f'''Você é um especialista em mídia social, com foco em ajudar marcas a maximizar sua presença nas
+                                plataformas de mídia social com base em dados do mercado.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Especialista em Inbound Marketing",
+                                goal=f'''Desenvolver uma estratégia de inbound marketing para {nome_cliente}, com foco em atrair e converter leads.''',
+                                backstory=f'''Você é um especialista em inbound marketing, utilizando as melhores práticas 
+                                para atrair e engajar clientes em potencial para {nome_cliente}.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Especialista em SEO",
+                                goal=f'''Melhorar o SEO de {nome_cliente}, com base na análise do site ({performance_metrics_df}) e na concorrência .''',
+                                backstory=f'''Você é um especialista em SEO, com o objetivo de melhorar a visibilidade do site de {nome_cliente} 
+                                nos motores de busca, com base na análise do conteúdo existente e da concorrência.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Especialista em Criativos",
+                                goal=f'''Desenvolver criativos da campanha de {nome_cliente}, com base no {ramo_atuacao}, {intuito_plano} e {publico_alvo}.''',
+                                backstory=f'''Você é um especialista em Criativos de marketing digital, você é original, detalhista, 
+                                minucioso, criativo, com uma vasta experiência de mercado lidando com uma gama de empresas que atingiram sucesso por conta do seu 
+                                extenso repertório profissional, com o objetivo de trazer o máximo de atenção às campanhas do cliente. 
+                                Tornando-as relevantes e fazendo com que o cliente atinja seus objetivos.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            
+                            Agent(
+                                role="Especialista em CRM",
+                                goal=f'''Desenvolver estratégias de CRM para o cliente: {nome_cliente}.''',
+                                backstory=f'''Você é um especialista em CRM. você é original, detalhista, minucioso, 
+                                criativo, com uma vasta experiência de mercado lidando com uma gama de empresas que atingiram sucesso por conta do seu extenso 
+                                repertório profissional, Você sabe estabelecer relações durarouras com clientes e sabe tudo que há de se
+                                saber para detalhar planos de como firmar e continuar relacionamentos estratégicos com clientes.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            
+                            Agent(
+                                role="Especialista em Marca/Design",
+                                goal=f'''Desenvolver ideias de Marca/Design do cliente: {nome_cliente}.''',
+                                backstory=f'''Você é um especialista em Marca/Design, você é original, detalhista, minucioso, criativo, 
+                                com uma vasta experiência de mercado lidando com uma gama de empresas que atingiram sucesso por conta do seu extenso repertório profissional, 
+                                com o objetivo de melhorar a visibilidade de {nome_cliente} trazendo a sua marca
+                                de uma forma coerente e chamativa.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+
+                            Agent(
+                                role="Especialista em SEO",
+                                goal=f'''Melhorar o SEO de {nome_cliente}, com base na análise do site e na concorrência.''',
+                                backstory=f'''Você é um especialista em SEO, você é analítico, detalhista, minucioso, criativo, 
+                                com uma vasta experiência de mercado lidando com uma gama de empresas que atingiram sucesso por conta do seu extenso repertório 
+                                profissional, com o objetivo de melhorar a visibilidade do site de {nome_cliente} nos motores de busca, com base na análise do
+                                conteúdo existente e da concorrência.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                            Agent(
+                                role="Especialista em Redes Sociais",
+                                goal=f'''Estabelecer o plano de atuação em redes sociais de {nome_cliente} no planejamento estratégico, com base na análise do site e na concorrência.''',
+                                backstory=f'''Você é um especialista em marketing em redes sociais, 
+                                você é original, detalhista, minucioso, criativo, com uma vasta experiência de mercado lidando com uma gama de 
+                                empresas que atingiram sucesso por conta do seu extenso repertório profissional, com o objetivo de melhorar a visibilidade nas campanhas 
+                                {nome_cliente}, com base na análise do conteúdo existente e da concorrência.''',
+                                allow_delegation=False,
+                                llm=modelo_linguagem,
+                                tools=[]
+                            ),
+                        ]
 
                     
 
