b56d5dd9695a033f09b7248906d8a67f473b970b
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-19 15:00:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import os
 from tavily import TavilyClient
 from etapas.midias import planej_midias_page
 from etapas.crm import planej_crm_page
+from chat.rag import mistral_rag_app
 
 st.set_page_config(layout="wide",page_title="Macfor AutoDoc",
                   page_icon="static/page-icon.png")  
