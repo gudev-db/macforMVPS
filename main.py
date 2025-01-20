46a2aa83450b0481684ea1a4aa6b5040602fe5b0
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-20 08:24:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from etapas.midias import planej_midias_page
 from etapas.crm import planej_crm_page
 import google.generativeai as genai
 from contato.temaEmail import gen_temas_emails
-from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 
 st.set_page_config(layout="wide",page_title="Macfor AutoDoc",
