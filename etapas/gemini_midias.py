a13b5e83918c46bfefe4423f6f40780edc6f0146
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-23 10:04:13 -0300

    Update gemini_midias.py

--- etapas/gemini_midias.py
+++ etapas/gemini_midias.py
@@ -152,10 +152,11 @@ def planej_midias_page():
                         
                         - Extraia todo o seu conhecimento possível sobre marketing digital, estratégicas de campanhas:
                         
-                        - divida a estratégia de acordo com cada rede social (Instagram, Facebook, LinkedIn, WhatsApp, YouTube) no que se refere a alocação do orçamento: {budget} reais (alocação
-                        devida e detalhadamente justificada),
-                        tipos de campanhas a serem realizadas (ex: pesquisa, display, video, app, pmax) de acordo com a plataforma e porque. Em formato de cronograma e considerando datas
-                        comemorativas.
+                        - Em formato de cronograma e considerando datas comemorativas, divida a estratégia de acordo com cada rede social (Instagram, Facebook, LinkedIn, WhatsApp, YouTube) no que se refere a alocação do orçamento: {budget} reais (alocação
+                        devida e detalhadamente justificada) definindo tambem
+                        tipos de campanhas a serem realizadas (ex: pesquisa, display, video, app, pmax) de acordo com a plataforma e porque.
+                        
+                        
                         
 
 
