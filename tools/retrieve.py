7f58d43faea1f3bad111d28fb086ce625423b7ba
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-27 13:08:30 -0300

    Update retrieve.py

--- tools/retrieve.py
+++ tools/retrieve.py
@@ -6,50 +6,34 @@ def visualizar_planejamentos():
     client = MongoClient("mongodb+srv://gustavoromao3345:RqWFPNOJQfInAW1N@cluster0.5iilj.mongodb.net/")
     db = client['arquivos_planejamento']  # Nome do seu banco de dados
     collection = db['auto_doc']
-
-    # Teste de conexão
-    try:
-        client.admin.command('ping')
-        st.write("Conexão bem-sucedida com o MongoDB!")
-    except Exception as e:
-        st.write("Erro ao conectar ao MongoDB:", e)
-        return
     
     # Obter todos os documentos e verificar se há documentos
     planejamentos = collection.find({}, {"_id": 1, "id_planejamento": 1})  # Buscar apenas _id e id_planejamento
 
-    # Verificar se a coleção tem documentos
+    # Se não houver documentos registrados
     if collection.count_documents({}) == 0:
         st.write("Não há planejamentos registrados.")
         return
 
     # Listar 'id_planejamento' para o selectbox
-    id_planejamentos = [planejamento.get('id_planejamento', 'N/A') for planejamento in planejamentos]
+    id_planejamentos = [planejamento['id_planejamento'] for planejamento in planejamentos]
 
-    # Verificar se a lista de id_planejamentos não está vazia
-    if not id_planejamentos:
-        st.write("Não há 'id_planejamento' disponíveis para seleção.")
-        return
-
-    # Criar o selectbox para o usuário escolher o id_planejamento
+    # Criar o selectbox para o usuário escolher um id_planejamento
     selected_id = st.selectbox("Selecione um planejamento:", id_planejamentos)
-
-    # Mostrar o ID selecionado (apenas para depuração)
-    st.write(f"Você selecionou o id: {selected_id}")
 
     # Encontrar o planejamento selecionado
     selected_planejamento = collection.find_one({"id_planejamento": selected_id})
 
-    # Verificar se o planejamento foi encontrado
     if selected_planejamento:
-        # Exibindo todos os campos do planejamento
+        # Exibindo todos os dados do planejamento de forma dinâmica
         st.header(f"Planejamento para: {selected_planejamento.get('tipo_plano', 'N/A')}")
         st.subheader(f"Cliente: {selected_planejamento.get('nome_cliente', 'N/A')}")
 
-        # Exibir todos os campos encontrados no documento
+        # Exibindo todos os campos do planejamento, exceto o _id
         for key, value in selected_planejamento.items():
             if key != "_id":  # Ignorar o campo "_id"
                 st.subheader(f"{key.replace('_', ' ').title()}:")
                 st.markdown(f"**{key.replace('_', ' ').title()}:** {value if value else 'N/A'}")
+
     else:
         st.write("Planejamento não encontrado.")
