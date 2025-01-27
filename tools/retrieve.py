5677fb23e088fe225ab6353735145ec3502a0961
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-27 13:40:05 -0300

    Update retrieve.py

--- tools/retrieve.py
+++ tools/retrieve.py
@@ -5,10 +5,9 @@ from pymongo import MongoClient
 def visualizar_planejamentos():
     # Conectar ao MongoDB
     client = MongoClient("mongodb+srv://gustavoromao3345:RqWFPNOJQfInAW1N@cluster0.5iilj.mongodb.net/")
-
     db = client['arquivos_planejamento']  # Substitua pelo nome do seu banco de dados
     collection = db['auto_doc']
-    
+
     # Obter todos os IDs dos documentos (presumindo que 'id_planejamento' seja o identificador único)
     planejamentos = collection.find({}, {"_id": 1, "id_planejamento": 1})  # Buscar apenas _id e id_planejamento
 
@@ -39,48 +38,48 @@ def visualizar_planejamentos():
             # Exibindo o nome do cliente e o tipo de plano
             st.header(f"Planejamento para: {selected_planejamento.get('tipo_plano')}")
             st.subheader(f"Cliente: {selected_planejamento.get('nome_cliente')}")
-            
+
             # Exibindo as seções do planejamento com subheaders e os dados respectivos
             st.header('1. Etapa de Pesquisa de Mercado')
             st.subheader('1.1 Análise SWOT')
             st.markdown(f"**SWOT:** {selected_planejamento.get('SWOT')}")
-            
+
             st.subheader('1.2 Análise PEST')
             st.markdown(f"**P:** {selected_planejamento.get('P')}")
             st.markdown(f"**E:** {selected_planejamento.get('E')}")
             st.markdown(f"**S:** {selected_planejamento.get('S')}")
             st.markdown(f"**T:** {selected_planejamento.get('T')}")
-            
+
             st.subheader('1.3 Tendências de Mercado')
             st.markdown(f"**Tendências:** {selected_planejamento.get('Tendencias')}")
-            
+
             st.header('2. Etapa Estratégica')
             st.subheader('2.1 Golden Circle')
             st.markdown(f"**GC:** {selected_planejamento.get('GC')}")
-            
+
             st.subheader('2.2 Posicionamento de Marca')
             st.markdown(f"**Posicionamento de Marca:** {selected_planejamento.get('Posicionamento_Marca')}")
-            
+
             st.subheader('2.3 Brand Persona')
             st.markdown(f"**Brand Persona:** {selected_planejamento.get('Brand_Persona')}")
-            
+
             st.subheader('2.4 Buyer Persona')
             st.markdown(f"**Buyer Persona:** {selected_planejamento.get('Buyer_Persona')}")
-            
+
             st.subheader('2.5 Tom de Voz')
             st.markdown(f"**Tom de Voz:** {selected_planejamento.get('Tom_Voz')}")
-            
+
             st.header('3. Etapa de Planejamento de Mídias e Redes Sociais')
             st.subheader('3.1 Visual')
             st.markdown(f"**Estruturação do KV:** {selected_planejamento.get('KV')}")
             st.markdown(f"**Plano de Redes Sociais:** {selected_planejamento.get('Plano_Redes')}")
-            
+
             st.subheader('3.2 Plano para Criativos')
             st.markdown(f"**Criativos:** {selected_planejamento.get('Plano_Criativos')}")
-            
+
             st.subheader('3.3 Plano de SEO')
             st.markdown(f"**Plano de Palavras Chave:** {selected_planejamento.get('Plano_Palavras_Chave')}")
-            
+
             st.subheader('3.4 Plano de CRM')
             st.markdown(f"**Estratégia Geral de CRM:** {selected_planejamento.get('Plano_Estrategia_CRM')}")
             st.markdown(f"**Fluxo Geral de CRM:** {selected_planejamento.get('Plano_Fluxo_CRM')}")
@@ -90,3 +89,4 @@ def visualizar_planejamentos():
             st.markdown(f"**Softwares Recomendados:** {selected_planejamento.get('Recomend_Software')}")
         else:
             st.write("Planejamento não encontrado.")
+
