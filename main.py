7f55783187d3f08e034dd2876e6cbb4b0c54b3ce
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-01-29 15:40:17 +0000

    return to form after issues

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import streamlit as st
-#from crewai import Agent, Task, Process, Crew
-#from langchain_openai import ChatOpenAI
+from crewai import Agent, Task, Process, Crew
+from langchain_openai import ChatOpenAI
 from datetime import datetime
 from etapas.gemini_mkt import planej_mkt_page
 from tools.retrieve import visualizar_planejamentos  # Importando a função visualizar_planejamentos
