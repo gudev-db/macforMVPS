d123dbe11df4609383b7f6c5c2563857be0d3d7f
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-02-03 12:11:19 +0000

    melhorias na geração de temas de email

--- contato/temaEmail.py
+++ contato/temaEmail.py
@@ -1,10 +1,12 @@
 import os
 import streamlit as st
 import google.generativeai as genai
+import requests
 
 # Configure the Gemini API
 gemini_api_key = os.getenv("GEM_API_KEY")
 genai.configure(api_key=gemini_api_key)
+rapid_key = os.getenv("RAPID_API")
 
 # Function to generate email themes and display results
 def gen_temas_emails():
@@ -18,26 +20,55 @@ def gen_temas_emails():
         "Referência da Marca:",
         height=200  
     )
+    tipo_empresa = st.selectbox("Escolha o tipo de empresa:", ["B2B", "B2C", "B2G"])
+
+    servicos_empresa = st.text_input("Digite aqui os tipos de serviços que a empresa realiza:")
+
+    stakeholders = st.text_input("Stakeholders da comunicação:")
+
     
     st.subheader("Informações do(s) destinatário(s)")
-    destinatarios = st.text_input("Caracterize os destinatários:")
+    destinatarios = st.text_input("Caracterize os destinatários quanto a sua segmentação:")
     
     if st.button("Gerar Temas de Emails"):
         if not nome_cliente or not ramo_atuacao or not destinatarios:
             st.warning("Por favor, preencha todas as informações.")
         else:
             with st.spinner("Brainstorming..."):
+
+
+                url = "https://duckduckgo8.p.rapidapi.com/"
+                            
+                querystring = {"q":f"tendencias e novidades em {ramo_atuacao}"}
+                            
+                headers = {
+                                "x-rapidapi-key": rapid_key,
+                                "x-rapidapi-host": "duckduckgo8.p.rapidapi.com"
+                            }
+                            
+                response = requests.get(url, headers=headers, params=querystring)
+                            
+                tend_novids2 = response.text
+                
+
                 # Generate email themes using the Gemini model
                 prompt = f"""
                 Crie um cronograma anual de temas de emails específicos para CADA UMA das segmentações de leads conforme {destinatarios} com fins de nutrição de leads. 
-                O remetente dos emails é {nome_cliente}, que atua no ramo de {ramo_atuacao}. 
+                O remetente dos emails é {nome_cliente}, que atua no ramo de {ramo_atuacao}. Eis uma breve descrição sobre a marca: {referencia_da_marca}.
+                A empresa costuma vender serviços como {servicos_empresa}.;
 
-                Extraia de todo o seu conhecimento as especificidades de cada segmento dos destinatários e entenda o que de fato, em cada mês, iria pegar as suas atenções
-                no que se diz respeito a datas comemorativas e características locais. Engaja, nutra, conquiste os leads.
+                Os stakeholders da comunicação são: {stakeholders}.;
+
+                A empresa é do tipo {tipo_empresa}.;
+
+                Considere as novidades do setor de atuação em: {tend_novids2};
                 
-                Considere incluir datas comemorativas relevantes e temáticas gerais apropriadas ao público-alvo.
+                Extraia de todo o seu conhecimento as especificidades de cada segmento dos destinatários e entenda o que de fato, em cada mês, iria pegar as suas atenções
+                no que se diz respeito a datas comemorativas e características locais. Engaja, nutra, conquiste os leads.;
+                
+                Considere incluir datas comemorativas relevantes e temáticas gerais apropriadas ao público-alvo.;
                 Você segue o princípio de atuação GLOCAL. Pensar GLOBALMENTE e agir LOCALMENTE. Organize a saída em formato de tabela. Se atente às especificidades de cada segmentação.
-                Seja claro, preciso e prático. Não me dê diretrizes, crie os temas que eu preciso para eu colocar diretamente em produção.
+                Seja claro, preciso e prático. Não me dê diretrizes, crie os temas que eu preciso para eu colocar diretamente em produção.;
                 """
                 try:
                     response = llm.generate_content(prompt)
