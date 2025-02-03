066259dbfffead159020602dc1f200bdb959a5ee
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-02-03 14:58:56 +0000

    prompts tema email melhorados

--- contato/Email.py
+++ contato/Email.py
@@ -37,7 +37,7 @@ def gen_emails():
                 prompt = f"""
                 Redija 5 emails partindo do destinatário {nome_cliente}, para o destinatário {dest_email} com o tema {tema} (o tema deve ter um profundo impacto na composição do email) assumindo o tom {tom}.
                 Instigue interação. Faça uma síntese das informaçoes dadas e redija o melhor email possível de uma forma que garanta a retenção do lead.
-
+                Separe e organize os emails com linhas entre eles.
 
                 """
                 try:
