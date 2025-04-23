167a851f27f5a73b5d51d8f434b7b323d7a2bbbf
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-04-23 02:50:56 +0000

    compartimentalizando funções de agentes

--- etapas/gemini_mkt.py
+++ etapas/gemini_mkt.py
@@ -267,7 +267,7 @@ def planej_mkt_page():
                         model="gemini-2.0-flash",
                         contents=[f'''
 
-                            Torne a seguinte análise SWOT menos genérica e mais relevante:{pre_SWOT_output}''']).text
+                            Reescreva a seguinte análise SWOT menos genérica e mais relevante:{pre_SWOT_output}''']).text
                         
                         
 
@@ -292,7 +292,7 @@ def planej_mkt_page():
 
                         concorrencias_output = client.models.generate_content(
                         model="gemini-2.0-flash",
-                        contents=[f'''Torne a seguinte análise de concorrência menos genérica, melhor redijida e mais relevante: {pre_concorrencias_output}''']).text
+                        contents=[f'''Reescreva a seguinte análise de concorrência menos genérica, melhor redijida e mais relevante: {pre_concorrencias_output}''']).text
 
 
 
@@ -312,7 +312,7 @@ def planej_mkt_page():
 
                         PEST_output = client.models.generate_content(
                         model="gemini-2.0-flash",
-                        contents=[f'''Torne a seguinte análise PEST menos genérica, melhor redijida e mais relevante: {pre_PEST_output}''']).text
+                        contents=[f'''Reescreva a seguinte análise PEST menos genérica, melhor redijida e mais relevante: {pre_PEST_output}''']).text
 
 
                         prompt_golden = f'''
@@ -339,7 +339,7 @@ def planej_mkt_page():
 
                         golden_output = client.models.generate_content(
                         model="gemini-2.0-flash",
-                        contents=[f'''Torne o seguinte Golden Circle menos genérico, melhor redijido, com mais impacto: {pre_golden_output}''']).text
+                        contents=[f'''Reescreva o seguinte Golden Circle menos genérico, melhor redijido, com mais impacto: {pre_golden_output}''']).text
 
                         prompt_posicionamento = f'''
                             
@@ -391,7 +391,7 @@ def planej_mkt_page():
 
                         posicionamento_output = client.models.generate_content(
                         model="gemini-2.0-flash",
-                        contents=[f'''Torne o seguinte posicionamento de marca menos genérico, de melhor qualidade, com mais impacto: {pre_posicionamento_output}''']).text
+                        contents=[f'''Reescreva o seguinte posicionamento de marca menos genérico, de melhor qualidade, com mais impacto: {pre_posicionamento_output}''']).text
 
 
                         prompt_brand_persona = f'''2 Brand Personas detalhada, alinhada com a marca do {nome_cliente} que é do setor de atuação {ramo_atuacao} em português brasileiro considerando o 
