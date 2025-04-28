30b759583ef7ab08cc93fb5bfbcc82ccdd9d60f8
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-04-28 13:30:33 +0000

    sistema de melhoria aplicado ao KV

--- etapas/gemini_midias.py
+++ etapas/gemini_midias.py
@@ -238,7 +238,7 @@ def planej_midias_page():
                                   {prompt_kv_aval}
                                   ###Fim da avaliação do Key visual###
 
-                                  Adapte o Key Visual Abaixo para que ele atenda a avaliação. Reescreva o Key visual inteiro levando em conta a avaliação. Não aponte o que você mudou, apenas traga o novo Key Visual.
+                                  Com base na avaliação, reescreva o Key Visual Seguinte de uma forma que atenda a avaliação. Não me diga o que você mudou, apenas escreva o novo Key Visual.
 
                                   ##Key Visual a ser avaliado##
                                   {kv_output}
