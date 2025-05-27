e64ffa5e13fd4473e6cb9ed727cced02833160d4
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-05-27 09:53:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from contato.noticias import pesquisa
 from etapas.gemini_trend import gerar_trend
 from etapas.seo_gemini import seo_page
 from etapas.gemini_redes import planej_redes_page
+from etapas.rede_agente import nn_gen
 
 st.set_page_config(
     layout="wide",
@@ -136,7 +137,8 @@ if login():
             [
                 "Selecione uma opção",
                 "Planejamento de Pesquisa e Estratégia",
-                "Pesquisa de Tendências"
+                "Pesquisa de Tendências",
+                "Geração de prompt"
             ]
         )
 
@@ -145,6 +147,8 @@ if login():
                 planej_mkt_page()
             elif pesquisa_estrategia == "Pesquisa de Tendências":
                 pesquisa()
+            elif pesquisa_estrategia == "Geração de prompt":
+                nn_gen()
 
     # Seção para "Cliente"
     elif selecao_sidebar == "CRM":
